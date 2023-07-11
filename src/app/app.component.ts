f650a54f8a4b6be76f4c57bd5cb56b73f5d932ac
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-07-11 00:10:31 -0400

    feat: create header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SearchBarComponent } from './shared/components/search-bar/search-bar.component';
+import { IconComponent } from './shared/components/icon/icon.component';
+import { SortBtnComponent } from './shared/components/sort-btn/sort-btn.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SearchBarComponent, IconComponent, SortBtnComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ws-thinking-reactively-pokedex';
